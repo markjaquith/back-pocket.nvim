d9c619ee004fd1b6141b37ab7783b207de1ee591
Mark Jaquith <mark.jaquith@gusto.com>
2025-04-21 16:22:34 -0400

    Init

---
+++ README.md
@@ -0,0 +1,74 @@
+# back-pocket.nvim
+
+Back Pocket is a simple Neovim plugin that provides a command palette for commands that you find yourself using sometimes, but not frequently enough to assign or memorize a keybinding for. Or maybe you do use them a lot, but you don't need to run them quickly enough to justify a keybinding.
+
+Human brains can only remember so much, so it makes sense to have "tiers". Short keybindings for things you use all the time (your front pocket), and a place to put infrequently-used or lesser-importance items (your back pocket).
+
+## Requirements
+
+- Neovim 0.11 or later
+- snacks.nvim (Used for the command)
+
+## Installation
+
+### Lazy.nvim
+
+```lua
+{
+	"markjaquith/back-pocket.nvim",
+	lazy = true,
+  keys = {
+    {
+      '<leader>p', -- Customize your keybinding here
+      function()
+        require('back-pocket').choose()
+      end,
+      desc = 'Open Back Pocket command palette',
+    },
+  },
+  config = {
+		-- `items` can be a table, or a function that returns a table
+		-- If you use a function, it will be called with a context object
+		-- that has the following:
+		--
+		--   - get_git_branch()
+		--   - in_git_repo()
+		--   - get_github_url()
+		--   - copy(text)
+		--   - file
+		--   - path
+		--   - relative_path
+		--   - absolute_path
+    items = function(ctx)
+			local items = {
+				{
+					name = 'Greet',
+					description = 'Example command that greets the user',
+					command = function () vim.notify('Hello, world!') end,
+				}
+			}
+			local git_items = {
+        {
+          name = 'Copy Git Branch Name',
+          text = ctx.get_git_branch(),
+          command = function()
+            ctx.copy(ctx.get_git_branch())
+          end,
+        },
+			}
+			
+			if ctx.in_git_repo() then
+				table.insert(items, {
+					name = 'Git Status',
+					description = 'Show git status',
+					command = function() vim.cmd('Git') end,
+				})
+			end
+		end,
+	},
+}
+```
+
+## License
+
+back-pocket.nvim is licensed under the MIT License. See LICENSE for more information.
---
+++ lua/back-pocket/init.lua
@@ -0,0 +1,106 @@
+local M = {}
+
+local config = {
+	items = {},
+}
+
+M.setup = function(opts)
+	config = vim.tbl_deep_extend('force', config, opts or {})
+end
+
+M.choose = function()
+	local Snacks = require 'snacks'
+	local file = vim.fn.expand '%:t'
+	local path = vim.fn.expand '%:p'
+	local relative_path = path:gsub(vim.fn.getcwd(), ''):sub(2)
+	local absolute_path = relative_path
+	local home = vim.env.HOME
+	if path:sub(1, #home) == home then
+		absolute_path = '~' .. path:sub(#home + 1)
+	end
+
+	local function copy(text, msg)
+		msg = msg or text
+		vim.fn.setreg('+', text)
+		Snacks.notify(msg, { title = 'Clipboard' })
+	end
+
+	local function in_git_repo()
+		vim.fn.system('git rev-parse --git-dir > /dev/null 2>&1')
+
+		return vim.v.shell_error == 0
+	end
+
+	local function get_git_branch()
+		if not in_git_repo() then return '' end
+		return vim.fn.system('git rev-parse --abbrev-ref HEAD'):gsub('%s+$', '')
+	end
+
+	local function get_github_url()
+		if not in_git_repo() then return nil end
+
+		local remote_list = vim.fn.systemlist('git config --get remote.origin.url')
+
+		if vim.v.shell_error ~= 0 or #remote_list == 0 then return nil end
+
+		local remote = remote_list[1]
+
+		-- Improved parsing for different git URL formats (SSH, HTTPS)
+		local user_repo = remote:match('.*[:/]([^/]+/[^/]+)%.git$') or remote:match('.*[:/]([^/]+/[^/]+)$')
+
+		if not user_repo then return nil end
+
+		local branch = get_git_branch()
+		if branch == '' then return 'main' end
+
+		local line = vim.fn.line '.'
+		return string.format('https://github.com/%s/blob/%s/%s#L%d', user_repo, branch, relative_path, line)
+	end
+
+	local context = {
+		copy = copy,
+		get_github_url = get_github_url,
+		file = file,
+		path = path,
+		relative_path = relative_path,
+		absolute_path = absolute_path,
+		get_git_branch = get_git_branch,
+		in_git_repo = in_git_repo,
+	}
+
+	local items
+	if type(config.items) == 'function' then
+		items = config.items(context)
+	else
+		items = config.items
+	end
+
+	local items_table = {}
+	local max_name_length = 0
+
+	for i, item in ipairs(items) do
+		max_name_length = math.max(max_name_length, #item.name)
+		table.insert(items_table,
+			{ idx = i, score = i, name = item.name, text = item.name .. ' ' .. item.text, display_text = item.text })
+	end
+
+	Snacks.picker {
+		title = 'Commands',
+		items = items_table,
+		format = function(item)
+			local spacing = string.rep(' ', max_name_length - #item.name + 2)
+			return {
+				{ item.name,         'SnacksItemName' },
+				{ spacing },
+				{ item.display_text, 'Comment' },
+			}
+		end,
+		layout = 'select',
+		confirm = function(picker, item)
+			picker:close()
+			items[item.idx].command()
+		end,
+	}
+end
+
+return M
